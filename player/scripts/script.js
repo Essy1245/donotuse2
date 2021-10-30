ce2f1c899e82b2cdf20824d0e8250e30401dadd3
Seongchan Lee <80929407+Essy1245@users.noreply.github.com>
2021-10-29 19:13:11 -0700

    Update script.js

--- player/scripts/script.js
+++ player/scripts/script.js
@@ -14,7 +14,7 @@ new Vue({
           artist: "ITZY",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
           source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+          url: "https://youtu.be/nQKIWy27zN4",
           favorited: false
         },
         {
